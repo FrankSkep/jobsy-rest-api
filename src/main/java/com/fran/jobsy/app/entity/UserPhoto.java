206c5ec6e6dd5886e11d8f12d5a38eb49de1fc92
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-09-07 13:51:16 -0700

    Add: Image upload service with Cloudinary

--- src/main/java/com/fran/jobsy/app/entity/UserPhoto.java
+++ src/main/java/com/fran/jobsy/app/entity/UserPhoto.java
@@ -1,6 +1,7 @@
 package com.fran.jobsy.app.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +18,13 @@ public class UserPhoto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    private String imageId;
+
+    @NotBlank
+    private String url;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    private String url;
 }
---
+++ src/main/java/com/fran/jobsy/app/service/impl/CloudinaryServiceImpl.java
@@ -0,0 +1,28 @@
+package com.fran.jobsy.app.service.impl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.fran.jobsy.app.service.CloudinaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CloudinaryServiceImpl implements CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    @Override
+    public Map upload(MultipartFile file) throws IOException {
+        return cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+    }
+
+    @Override
+    public void delete(String publicId) throws IOException {
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+}
