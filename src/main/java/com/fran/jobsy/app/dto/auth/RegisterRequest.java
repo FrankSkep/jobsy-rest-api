9c789b54c296cb1d5a9f5ac3a6b3a262389333da
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-09-13 21:11:07 -0700

    fix: Allow tildes in RegisterRequestDTO validations

--- src/main/java/com/fran/jobsy/app/dto/auth/RegisterRequest.java
+++ src/main/java/com/fran/jobsy/app/dto/auth/RegisterRequest.java
@@ -32,7 +32,7 @@ public class RegisterRequest {
     private String lastname;
 
     @NotBlank(message = "Country is mandatory")
-    @Pattern(regexp = "^[A-Za-z ]+$", message = "Country must contain only letters and spaces")
+    @Pattern(regexp = "^[\\p{L} ]+$", message = "Country must contain only letters (including accents) and spaces")
     @Size(max = 100, message = "Country must not exceed 100 characters")
     private String country;
 }
