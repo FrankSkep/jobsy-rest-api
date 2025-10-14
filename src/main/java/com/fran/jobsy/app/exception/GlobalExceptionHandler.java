e2eec8005849fe8b12b651979c6063b0062abcdf
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-10-13 18:32:54 -0700

    handle HttpMethodNotSupported Exception

--- src/main/java/com/fran/jobsy/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fran/jobsy/app/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -164,5 +165,17 @@ public class GlobalExceptionHandler {
         );
         error.setPath(request.getRequestURI());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                           HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.METHOD_NOT_ALLOWED.value(),
+                ex.getMessage()
+        );
+        error.setPath(request.getRequestURI());
+        return new ResponseEntity<>(error, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
