766bd1270cfdc82bba0de77b12970fce821dded6
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-10-21 09:42:40 -0700

    health controller

---
+++ src/main/java/com/fran/jobsy/app/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.fran.jobsy.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthController {
+
+    @RequestMapping("/check")
+    public String checkHealth() {
+        return "OK";
+    }
+
+
+}
