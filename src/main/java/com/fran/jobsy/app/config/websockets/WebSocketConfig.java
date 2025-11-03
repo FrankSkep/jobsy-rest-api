0084cc9fd83a2270c6193fe62f4de36e2a68530c
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-11-03 10:00:34 -0800

    fix url allowed in websockets

--- src/main/java/com/fran/jobsy/app/config/websockets/WebSocketConfig.java
+++ src/main/java/com/fran/jobsy/app/config/websockets/WebSocketConfig.java
@@ -30,7 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .addInterceptors(jwtHandshakeInterceptor)
                 .setHandshakeHandler(userHandshakeHandler)
-                .setAllowedOrigins("http://localhost:5173", "https://jobsy-two.vercel.app.com")
+                .setAllowedOrigins("http://localhost:5173", "https://jobsy-two.vercel.app")
                 .withSockJS();
     }
 }
