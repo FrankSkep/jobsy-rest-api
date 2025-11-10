447a0adfe486165f03dacdfd5a2736fb409be873
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-11-10 08:56:47 -0800

    add new frontend url frontend

--- src/main/java/com/fran/jobsy/app/config/security/SecurityConfig.java
+++ src/main/java/com/fran/jobsy/app/config/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:5173",
-                            "https://jobsy-two.vercel.app"));
+                            "https://jobsy-app.vercel.app"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                     config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                     config.setAllowCredentials(true);
